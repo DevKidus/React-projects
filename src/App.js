0b8e70c911ba46dbc27f3226487dbdae499e5e8e
Kidus Abrham <dev.kidusabrham@gmail.com>
2021-08-30 16:20:07 +0300

    Color Generator Randomly

--- src/App.js
+++ src/App.js
@@ -1,4 +1,29 @@
+import { useState } from "react";
+
 const App = () => {
-  return <div>App</div>;
+  let [color, setColor] = useState("rgb(0, 0, 0)");
+  return (
+    <div
+      onClick={() => {
+        let numOne = Math.trunc(Math.random() * 255);
+        let numTwo = Math.trunc(Math.random() * 255);
+        let numThree = Math.trunc(Math.random() * 255);
+
+        setColor(`rgb(${numOne}, ${numTwo}, ${numThree})`);
+      }}
+      className="grid place-content-center text-center cursor-pointer"
+      style={{
+        backgroundColor: color,
+        width: "100%",
+        height: "100vh",
+        color: "#fff",
+      }}
+    >
+      <h1 className="text-3xl font-bold">
+        Click On The Background to Change Color{" "}
+      </h1>
+      <p className="text-2xl font-bold my-14">{color}</p>
+    </div>
+  );
 };
 export default App;
